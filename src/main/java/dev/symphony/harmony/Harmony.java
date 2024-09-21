4bd2a61d8e08bda339c66c5f67bb8333e920cd86
flatkat <fernan181pro@gmail.com>
2024-09-21 02:57:34 +0200

    -Set up MidnightLib -Started working on making StewStack configurable

--- src/main/java/dev/symphony/harmony/Harmony.java
+++ src/main/java/dev/symphony/harmony/Harmony.java
@@ -1,5 +1,7 @@
 package dev.symphony.harmony;
 
+import dev.symphony.harmony.config.HarmonyConfig;
+import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.util.Identifier;
@@ -17,5 +19,7 @@ public class Harmony implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// be gay, do crime
+		MidnightConfig.init("harmony", HarmonyConfig.class);
+
 	}
 }
