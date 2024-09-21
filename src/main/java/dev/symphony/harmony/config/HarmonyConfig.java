fe446ef2f611ed28bfaea725ab3745d414f267ca
flatkat <flatkat181@gmail.com>
2024-09-21 12:07:42 +0200

    Make Stew Stack Size configurable (Straight up disabling this feature doesnt work yet)

--- src/main/java/dev/symphony/harmony/config/HarmonyConfig.java
+++ src/main/java/dev/symphony/harmony/config/HarmonyConfig.java
@@ -6,6 +6,7 @@ public class HarmonyConfig extends MidnightConfig{
 
     public static final String FOOD = "Food";
 
-    @MidnightConfig.Entry(category = FOOD) public static boolean StewStack = true;
+    @MidnightConfig.Entry(category = FOOD) public static boolean StewStackBool = true;
+    @MidnightConfig.Entry(category = FOOD) public static int StewStackSize = 16;
 
 }
