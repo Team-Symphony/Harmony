0ac0b8c2ae3a9f3bcadcaaa3241aac6abdb28584
flatkat <flatkat181@gmail.com>
2024-09-24 21:32:58 +0200

    Added a lil reminder as a comment so i dont forget abt stuff. Also where are my silly comments on the code? I demand them!

--- src/main/java/dev/symphony/harmony/ModTags.java
+++ src/main/java/dev/symphony/harmony/ModTags.java
@@ -5,6 +5,6 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 
 public class ModTags {
+    // Add documentation abt data-driven vehicles tag maybe :P
     public static final TagKey<EntityType<?>> VEHICLES = TagKey.of(RegistryKeys.ENTITY_TYPE, Harmony.id("vehicles"));
-
 }
