f97c188b68063c8e390a4d6c0c96991466d5dcd9
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-10-03 17:27:08 +0200

    Remove debug recipes and add saddle recipe

--- src/main/java/dev/symphony/harmony/config/HarmonyConfig.java
+++ src/main/java/dev/symphony/harmony/config/HarmonyConfig.java
@@ -11,15 +11,9 @@ public class HarmonyConfig extends MidnightConfig {
     @Entry(category = TRANS) public static boolean exitVehicleOnDamage = true;
     @Entry(category = TRANS) public static boolean vehiclesMoveThroughLeaves = true;
     @Entry(category = TRANS, isSlider = true, min = 0f, max = 1f) public static float leafSpeedFactor = 0.85f;
+    @HarmonyConfigCondition.ResourceConfigName(config_name = "recipe/saddle") @Entry(category = TRANS) public static boolean saddleRecipe = true;
 
     // Food
     public static final String FOOD = "food";
     @Entry(category = FOOD) public static int stewStackSize = 16;
-
-    // Recipe
-    public static final String RECIPE = "recipe";
-    @HarmonyConfigCondition.ResourceConfigName(config_name = "stick")
-    @Entry(category = RECIPE) public static boolean enableStickRecipe = false;
-    @HarmonyConfigCondition.ResourceConfigName(config_name = "stone_stick")
-    @Entry(category = RECIPE) public static boolean enableStoneStickRecipe = true;
 }
