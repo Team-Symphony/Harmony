344051c9fc777f45b28d702e4e4b8b563ea67d34
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-10-05 23:34:21 +0200

    Changed diamond horse armor preventRageChance back to 0.9 as it is the proper value. -Also changed the name of rageChance variable to preventRageChance to properly reflect what it does.

--- src/main/java/dev/symphony/harmony/mixin/transportation/HorseArmorBucking.java
+++ src/main/java/dev/symphony/harmony/mixin/transportation/HorseArmorBucking.java
@@ -19,22 +19,22 @@ import java.util.Map;
 @Mixin (AbstractHorseEntity.class)
 public class HorseArmorBucking {
     @Unique
-    private static final Map<Item, Float> rageChance = new Object2FloatArrayMap<>();
+    private static final Map<Item, Float> preventRageChance = new Object2FloatArrayMap<>();
 
     @Shadow @Final private Inventory inventory;
 
     static {
         // TODO: Add netherite horse armor if melody is loaded with a value of 1F
-        rageChance.put(Items.DIAMOND_HORSE_ARMOR, 1F); // if value here is 1F, it is for debugging, it's meant to be 0.9F
-        rageChance.put(Items.IRON_HORSE_ARMOR, 0.75F);
-        rageChance.put(Items.GOLDEN_HORSE_ARMOR, 0.6F);
-        rageChance.put(Items.LEATHER_HORSE_ARMOR, 0.45F);
+        preventRageChance.put(Items.DIAMOND_HORSE_ARMOR, 0.9F);
+        preventRageChance.put(Items.IRON_HORSE_ARMOR, 0.75F);
+        preventRageChance.put(Items.GOLDEN_HORSE_ARMOR, 0.6F);
+        preventRageChance.put(Items.LEATHER_HORSE_ARMOR, 0.45F);
     }
 
     @Inject(method = "updateAnger", at = @At("HEAD"), cancellable = true)
     private void rejectAngryWhenDrip(CallbackInfo ci) {
         ItemStack armor = this.inventory.getStack(0);
-        float chance = rageChance.getOrDefault(armor.getItem(), 0F);
+        float chance = preventRageChance.getOrDefault(armor.getItem(), 0F);
         if (chance > 0 && chance < 1 || Math.random() <= chance) ci.cancel();
     }
 }
