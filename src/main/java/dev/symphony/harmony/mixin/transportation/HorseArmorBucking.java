df0e987c3c7d9e9d28c4ced81a22aeb672060e17
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-10-05 23:30:46 +0200

    Fixed horse armor not being properly detected.

--- src/main/java/dev/symphony/harmony/mixin/transportation/HorseArmorBucking.java
+++ src/main/java/dev/symphony/harmony/mixin/transportation/HorseArmorBucking.java
@@ -2,10 +2,11 @@ package dev.symphony.harmony.mixin.transportation;
 
 import it.unimi.dsi.fastutil.objects.Object2FloatArrayMap;
 import net.minecraft.entity.passive.AbstractHorseEntity;
-import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -20,8 +21,7 @@ public class HorseArmorBucking {
     @Unique
     private static final Map<Item, Float> rageChance = new Object2FloatArrayMap<>();
 
-    @Shadow
-    protected SimpleInventory items;
+    @Shadow @Final private Inventory inventory;
 
     static {
         // TODO: Add netherite horse armor if melody is loaded with a value of 1F
@@ -33,7 +33,7 @@ public class HorseArmorBucking {
 
     @Inject(method = "updateAnger", at = @At("HEAD"), cancellable = true)
     private void rejectAngryWhenDrip(CallbackInfo ci) {
-        ItemStack armor = items.getStack(1);
+        ItemStack armor = this.inventory.getStack(0);
         float chance = rageChance.getOrDefault(armor.getItem(), 0F);
         if (chance > 0 && chance < 1 || Math.random() <= chance) ci.cancel();
     }
