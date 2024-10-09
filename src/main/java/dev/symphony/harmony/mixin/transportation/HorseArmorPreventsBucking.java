b9b99ae4e7c9813adc29bdf15bf6df0689c19093
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-10-09 19:06:30 +0200

    Added Melody integration, making netherite horse armor completely prevent horses from bucking (temporary solution)

--- src/main/java/dev/symphony/harmony/mixin/transportation/HorseArmorPreventsBucking.java
+++ src/main/java/dev/symphony/harmony/mixin/transportation/HorseArmorPreventsBucking.java
@@ -2,11 +2,14 @@ package dev.symphony.harmony.mixin.transportation;
 
 import dev.symphony.harmony.config.HarmonyConfig;
 import it.unimi.dsi.fastutil.objects.Object2FloatArrayMap;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -29,7 +32,7 @@ public class HorseArmorPreventsBucking {
 
     static {
         // TODO: Make preventBuckingChance for every type of armor configurable
-        // TODO: Add netherite horse armor if melody is loaded with a default value of 1F
+        // TODO: Make this data-driven or configurable via config screen with a custom map config screen
         preventBuckingChance.put(Items.DIAMOND_HORSE_ARMOR, 0.9F);
         preventBuckingChance.put(Items.IRON_HORSE_ARMOR, 0.75F);
         preventBuckingChance.put(Items.GOLDEN_HORSE_ARMOR, 0.6F);
@@ -39,8 +42,13 @@ public class HorseArmorPreventsBucking {
     @Inject(method = "updateAnger", at = @At("HEAD"), cancellable = true)
     private void rejectAngryWhenDrip(CallbackInfo ci) {
         if(HarmonyConfig.horseArmorPreventsBucking){
+            if(FabricLoader.getInstance().isModLoaded("melody") && preventBuckingChance.get(Registries.ITEM.get(Identifier.of("melody:netherite_horse_armor"))) == null) {
+                // Temporary solution until we move this to a better, configurable system
+                preventBuckingChance.put(Registries.ITEM.get(Identifier.of("melody:netherite_horse_armor")), 1F);
+            }
             ItemStack armor = this.inventory.getStack(0);
             float chance = preventBuckingChance.getOrDefault(armor.getItem(), 0F);
+            System.out.println(preventBuckingChance);
             if (chance > 0 && chance < 1 || Math.random() <= chance) ci.cancel();
         }
     }
