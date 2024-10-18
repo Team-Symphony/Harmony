801abda930e25ebf2917b8e247c2ca5d04cb7355
TrigamDev <trigambusiness@gmail.com>
2024-10-18 14:47:36 -0400

    The directory was wrong :skull: Anyways it's data-driven now (kinda)

--- src/main/java/dev/symphony/harmony/mixin/transportation/HorseArmorPreventsBucking.java
+++ src/main/java/dev/symphony/harmony/mixin/transportation/HorseArmorPreventsBucking.java
@@ -4,8 +4,6 @@ import dev.symphony.harmony.Harmony;
 import dev.symphony.harmony.config.HarmonyConfig;
 import dev.symphony.harmony.entity.equipment.Equipment;
 import dev.symphony.harmony.registry.HarmonyRegistries;
-import it.unimi.dsi.fastutil.objects.Object2FloatArrayMap;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Item;
@@ -14,7 +12,6 @@ import net.minecraft.item.Items;
 import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.EntityView;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -24,6 +21,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Mixin (AbstractHorseEntity.class)
@@ -31,18 +29,17 @@ public abstract class HorseArmorPreventsBucking {
     // FEATURE:
     // AUTHORS: KikuGie, Flatkat
     @Unique
-    private static final Map<Item, Float> preventBuckingChance = new Object2FloatArrayMap<>();
+    private static final Map<Item, Identifier> equipments = new HashMap<>();
 
     @Shadow @Final private Inventory inventory;
-    @Shadow public abstract EntityView getWorld();
 
     static {
         // TODO: Make preventBuckingChance for every type of armor configurable
         // TODO: Make this data-driven or configurable via config screen with a custom map config screen
-        preventBuckingChance.put(Items.DIAMOND_HORSE_ARMOR, 0.9F);
-        preventBuckingChance.put(Items.IRON_HORSE_ARMOR, 0.75F);
-        preventBuckingChance.put(Items.GOLDEN_HORSE_ARMOR, 0.6F);
-        preventBuckingChance.put(Items.LEATHER_HORSE_ARMOR, 0.45F);
+        equipments.put(Items.DIAMOND_HORSE_ARMOR, Harmony.id("diamond"));
+        equipments.put(Items.IRON_HORSE_ARMOR, Harmony.id("iron"));
+        equipments.put(Items.GOLDEN_HORSE_ARMOR, Harmony.id("golden"));
+        equipments.put(Items.LEATHER_HORSE_ARMOR, Harmony.id("leather"));
     }
 
     @Inject(method = "updateAnger", at = @At("HEAD"), cancellable = true)
@@ -50,28 +47,27 @@ public abstract class HorseArmorPreventsBucking {
         AbstractHorseEntity thisHorse = (AbstractHorseEntity) (Object) this;
 
         if (HarmonyConfig.horseArmorPreventsBucking) {
-            if (Harmony.isMelodyPresent() && preventBuckingChance.get(Registries.ITEM.get(Identifier.of("melody:netherite_horse_armor"))) == null) {
+            if (Harmony.isMelodyPresent() && equipments.get(Registries.ITEM.get(Identifier.of("melody:netherite_horse_armor"))) == null) {
                 // Temporary solution until we move this to a better, configurable system
                 // This is inside rejectAngryWhenDrip because if not it gets called too early,
                 // so it can't detect Melody nor is the armor item registered yet, so it doesn't work.
                 // Yes, this is kind of a crappy workaround, but as stated earlier, its temporary
-                preventBuckingChance.put(Registries.ITEM.get(Identifier.of("melody:netherite_horse_armor")), 1F);
+                equipments.put(Registries.ITEM.get(Identifier.of("melody:netherite_horse_armor")), Harmony.id("netherite"));
             }
 
             ItemStack armor = this.inventory.getStack(0);
-            Harmony.LOGGER.info(String.valueOf(getPreventBuckingChance(thisHorse.getWorld(), armor)));
-            float cancelChance = preventBuckingChance.getOrDefault(armor.getItem(), 0F);
-            if (Math.random() <= cancelChance) ci.cancel();
+            float preventBuckingChance = getPreventBuckingChance(thisHorse.getWorld(), armor);
+            if (Math.random() <= preventBuckingChance) ci.cancel();
         }
     }
 
     @Unique
     public float getPreventBuckingChance(World world, ItemStack horseArmor) {
         final DynamicRegistryManager registryManager = world.getRegistryManager();
-        Equipment horseEquipment = registryManager.get(HarmonyRegistries.EQUIPMENT_KEY).get(Harmony.id("diamond"));
+        Identifier equipmentId = equipments.get(horseArmor.getItem());
+        Equipment horseEquipment = registryManager.get(HarmonyRegistries.EQUIPMENT_KEY).get(equipmentId);
 
         Harmony.LOGGER.info(String.valueOf(horseEquipment));
-        Harmony.LOGGER.info(String.valueOf(registryManager.get(HarmonyRegistries.EQUIPMENT_KEY).stream().toList()));
 
         if (horseEquipment == null) return 0F;
         return horseEquipment.horseEquipment().preventBuckingChance();
