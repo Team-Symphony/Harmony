c179dc89a5b12f971967533dcdd0d8d9c038dab6
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-10-26 12:39:27 +0200

    Deleted remappedSrc

--- remappedSrc/dev/symphony/harmony/mixin/mobs/permissive_parrot_perching/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Parrots are able to perch on players' shoulders more reliably, only flying off when the player touches water without being in a boat, takes damage, touches powder snow, flies with elytra or quickly sneaks and unsneaks.
- * @author axialeaa
- */
-package dev.symphony.harmony.mixin.mobs.permissive_parrot_perching;
--- remappedSrc/dev/symphony/harmony/mixin/mobs/wolves_growl_at_monsters/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Tamed wolves growl when there is a hostile mob nearby, regardless of whether that mob has made them "angry".
- * @author axialeaa
- */
-package dev.symphony.harmony.mixin.mobs.wolves_growl_at_monsters;
