6f88979a53196ab2713545fbc708280ce85ae228
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-11-26 19:55:13 +0100

    Thanks Powercyphe for explicitly allowing me to "steal" your code

---
+++ src/main/java/dev/symphony/harmony/mixin/combat/tridents_return_from_void/TridentEntityMixin.java
@@ -0,0 +1,32 @@
+package dev.symphony.harmony.mixin.combat.tridents_return_from_void;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TridentEntity.class)
+public abstract class TridentEntityMixin extends Entity {
+    @Shadow public int returnTimer;
+    @Shadow private boolean dealtDamage;
+
+    protected TridentEntityMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void tickMixin(CallbackInfo ci) {
+        if (!this.getWorld().isClient) {
+            if (!this.dealtDamage && this.getBlockPos().getY() < 1 && this.getWorld().getDimensionEntry().getKey().get().getValue().toString().contains("minecraft:the_end")) {
+                this.dealtDamage = true;
+                this.returnTimer = 0;
+            }
+        }
+    }
+}
