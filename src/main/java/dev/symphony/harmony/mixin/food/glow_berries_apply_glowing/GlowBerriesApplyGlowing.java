850491b2019599fab49bca5b254745258959f675
Flatkat <87091676+Flatkat@users.noreply.github.com>
2025-01-23 16:45:25 +0100

    Resolving more conflicts

--- src/main/java/dev/symphony/harmony/mixin/food/glow_berries_apply_glowing/GlowBerriesApplyGlowing.java
+++ src/main/java/dev/symphony/harmony/mixin/food/glow_berries_apply_glowing/GlowBerriesApplyGlowing.java
@@ -1,6 +1,6 @@
 package dev.symphony.harmony.mixin.food.glow_berries_apply_glowing;
 
-import dev.symphony.harmony.config.HarmonyConfig;
+import dev.symphony.harmony.Harmony;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -19,10 +19,10 @@ public class GlowBerriesApplyGlowing {
     @Inject(method = "finishUsing", at = @At("HEAD"))
     private void glowBerryGlowEffect(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
 
-        if(!world.isClient && HarmonyConfig.glowBerryEffect != 0) {
+        if(!world.isClient && Harmony.CONFIG.glowBerryEffect() != 0) {
             // If eating Glow Berry, apply glowing
             if (stack.getItem() == Items.GLOW_BERRIES) {
-                int glowDuration = HarmonyConfig.glowBerryEffect * 20;
+                int glowDuration = Harmony.CONFIG.glowBerryEffect() * 20;
                 user.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, glowDuration, 0, true, true));
             }
         }
